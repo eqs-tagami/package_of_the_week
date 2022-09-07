1702cde98699daa0e116a5ae3f8932f1b5408893
eqs-tagami <eqs.tagami@gmail.com>
2022-09-07 15:02:50 +0900

    カレンダーの例を実行してみた

---
+++ flutter-examples-master/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/shoto/fvm/versions/3.3.0"
+export "FLUTTER_APPLICATION_PATH=/Users/shoto/Documents/daichi_group/programming/flutter/package_of_the_week/flutter-examples-master"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/shoto/Documents/daichi_group/programming/flutter/package_of_the_week/flutter-examples-master/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=20.2.36"
+export "FLUTTER_BUILD_NUMBER=20.2.36"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/shoto/Documents/daichi_group/programming/flutter/package_of_the_week/flutter-examples-master/.dart_tool/package_config.json"
